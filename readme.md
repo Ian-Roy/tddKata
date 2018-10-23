499d5161a69d5a8804ec820f362428e20be4f9a1
Ian Roy <ianroydev@gmail.com>
2018-10-23 16:02:05 -0400

    new line delimiter

---
+++ readme.md
@@ -0,0 +1,2 @@
+i am using this command chain as a gated check in
+`python3 stringCalculator.py && git add -A . && git commit -m msg `
--- stringCalculator.py
+++ stringCalculator.py
@@ -2,8 +2,13 @@ import unittest
 
 def add(numbers):
     if numbers == '':
-        return 0 
-    numberList = numbers.split(',')
+        return 0
+    if '\n' in numbers:
+        numbers = numbers.replace('\n' , ',')
+    if ',' in numbers:
+        numberList = numbers.split(',')
+    else:
+        numberList=[numbers]
     sum = 0
     for x in numberList:
         sum += float(x)
@@ -13,12 +18,25 @@ def add(numbers):
 class Tests(unittest.TestCase):
     def test_empty_string(self):
         self.assertEqual(add(''),0)
-
+class TestStringMethods(unittest.TestCase):
     def test_one_value(self):
         self.assertEqual(add('1'),1)
 
+    def test_one_big_value(self):
+        self.assertEqual(add('123'),123)
+
     def test_two_values(self):
         self.assertEqual(add('1,2'),3)
+
+    def test_add_many(self):
+        self.assertEqual(add('1,2,3,4,5'),15)
+
+    def test_mix_delimiters(self):
+        self.assertEqual(add('1,2,3\n4\n5'),15)
+
+    def test_all_new_line(self):
+        self.assertEqual(add('1\n2\n3\n4\n5'),15)
+        
     
 
 if __name__ == '__main__':
