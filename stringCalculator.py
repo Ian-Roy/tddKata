b1172a999e09213659c55488a62b519c383a9ee3
Ian Roy <ianroydev@gmail.com>
2018-10-23 16:29:50 -0400

    new custom delimiter support

--- stringCalculator.py
+++ stringCalculator.py
@@ -1,14 +1,23 @@
 import unittest
 
 def add(numbers):
+    delim=','
+    if numbers[:2]=='//':
+        delim = numbers[numbers.index('//')+2 : numbers.index('\n')]
+        numbers = numbers[numbers.index('\n')+1:]
+        
     if numbers == '':
         return 0
+    
     if '\n' in numbers:
-        numbers = numbers.replace('\n' , ',')
-    if ',' in numbers:
-        numberList = numbers.split(',')
+        numbers = numbers.replace('\n' , delim)
+        
+    if delim in numbers:
+        numberList = numbers.split(delim)
+        
     else:
         numberList=[numbers]
+        
     sum = 0
     for x in numberList:
         sum += float(x)
@@ -28,6 +37,9 @@ class TestStringMethods(unittest.TestCase):
     def test_two_values(self):
         self.assertEqual(add('1,2'),3)
 
+    def test_many_float_values(self):
+        self.assertAlmostEqual(add('1,2.5,3.4'),6.9)
+
     def test_add_many(self):
         self.assertEqual(add('1,2,3,4,5'),15)
 
@@ -36,8 +48,12 @@ class TestStringMethods(unittest.TestCase):
 
     def test_all_new_line(self):
         self.assertEqual(add('1\n2\n3\n4\n5'),15)
-        
-    
+
+    def test_new_delimiter(self):
+        self.assertEqual(add('//;\n1;2;3;4;5'),15)
+
+    def test_long_delimiter(self):
+        self.assertEqual(add('//;;;\n1;;;2;;;3;;;4;;;5'),15)
 
 if __name__ == '__main__':
     unittest.main()
